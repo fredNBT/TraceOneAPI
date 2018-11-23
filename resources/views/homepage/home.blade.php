cc7ba3c6282f1d1f3b08b94cc22e7c6f80fa225f
Lennart Beumer <len55@outlook.com>
2018-11-23 07:35:25 +0100

    heropage

--- resources/views/homepage/home.blade.php
+++ resources/views/homepage/home.blade.php
@@ -81,21 +81,178 @@
     
     
     <link rel="stylesheet" href="{{ asset('/css/app.css') }}">
+    <link rel="stylesheet" href="{{ asset('/css/app.css') }}">
     
-    <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.3.1/jquery.min.js"></script>
+
+    
+    
+    
+
+    <link href='https://fonts.googleapis.com/css?family=Lobster|Open+Sans:400,400italic,300italic,300|Raleway:300,400,600' rel='stylesheet' type='text/css'>
+    <link rel="stylesheet" type="text/css" href="{{ asset('css/font-awesome.min.css') }}">
+    <link rel="stylesheet" type="text/css" href="{{ asset('css/bootstrap.min.css') }}">
+    <link rel="stylesheet" type="text/css" href="{{ asset('css/animate.css') }}">
+    <link rel="stylesheet" type="text/css" href="{{ asset('css/style.css') }}">
   </head>
-   
   <body >
+
+      <div class="content">
+          <div class="container wow fadeInUp delay-03s">
+            <div class="row">
+              <div class="logo text-center">
+                <h2>We Are Baking Something New!! Comming Soon<br>Closed Grid Power System</h2>
+              </div>
+      
+              <div id="countdown" data-wow-delay=".3s" data-date="Dec 26, 2016 06:00:00"></div>
+              <h2 class="subs-title text-center">Subscribe now to get Recent updates!!!</h2>
+              <div class="subcription-info text-center">
+                <form class="subscribe_form" action="#" method="post">
+                  <input required="" value="" placeholder="Enter your email..." class="email" id="email" name="email" type="email">
+                  <input class="subscribe" name="email" value="Subscribe!" type="submit">
+                </form>
+                <p class="sub-p">We Promise to never spam you.</p>
+              </div>
+            </div>
+          </div>
+          <section>
+            <div class="container">
+              <div class="row bort text-center">
+                <div class="social">
+                  <ul>
+                    <li>
+                      <a href=""><i class="fa fa-facebook"></i></a>
+                    </li>
+                    <li>
+                      <a href=""><i class="fa fa-twitter"></i></a>
+                    </li>
+                    <li>
+                      <a href=""><i class="fa fa-linkedin"></i></a>
+                    </li>
+                  </ul>
+                </div>
+              </div>
+            </div>
+          </section>
+          <section id="about" class="section-padding">
+            <div class="container">
+              <div class="row">
+                <div class="col-md-12 col-sm-12 text-center">
+                  <div class="about-title">
+                    <h2>About Us</h2>
+                    <p>At vero eos et accusamus et iusto odio dignissimos ducimus qui blanditiis praesentium </br>voluptatum deleniti atque corrupti quos dolores e</p>
+                  </div>
+                  <div class="col-md-3 col-sm-6 col-xs-12 wow fadeInUp delay-02s">
+                    <div class="img">
+                      <i class="fa fa-refresh"></i>
+                    </div>
+                    <h3 class="abt-hd">Our process</h3>
+                    <p>At vero eos et accusamus et iusto odio dignissimos ducimus qui blanditiis praesentium voluptatum deleniti atque corrupti quos dolores</p>
+                  </div>
+                  <div class="col-md-3 col-sm-6 col-xs-12 wow fadeInUp delay-04s">
+                    <div class="img">
+                      <i class="fa fa-eye"></i>
+                    </div>
+                    <h3 class="abt-hd">Our Vision</h3>
+                    <p>At vero eos et accusamus et iusto odio dignissimos ducimus qui blanditiis praesentium voluptatum deleniti atque corrupti quos dolores</p>
+                  </div>
+                  <div class="col-md-3 col-sm-6 col-xs-12 wow fadeInUp delay-06s">
+                    <div class="img">
+                      <i class="fa fa-cogs"></i>
+                    </div>
+                    <h3 class="abt-hd">Our Approach</h3>
+                    <p>At vero eos et accusamus et iusto odio dignissimos ducimus qui blanditiis praesentium voluptatum deleniti atque corrupti quos dolores</p>
+                  </div>
+                  <div class="col-md-3 col-sm-6 col-xs-12 wow fadeInUp delay-08s">
+                    <div class="img">
+                      <i class="fa fa-dot-circle-o"></i>
+                    </div>
+                    <h3 class="abt-hd">Our Objective</h3>
+                    <p>At vero eos et accusamus et iusto odio dignissimos ducimus qui blanditiis praesentium voluptatum deleniti atque corrupti quos dolores</p>
+                  </div>
+                </div>
+              </div>
+            </div>
+          </section>
+          <div id="contact-info">
+            <div class="container">
+              <div class="row">
+                <div class="col-md-6">
+                  <div class="contact-title">
+                    <i class="fa fa-envelope"></i>
+                    <h2>Get in touch</h2>
+                    <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor <br>incididunt ut labore et dolore magna aliqua.</p>
+                  </div>
+                </div>
+                <div class="contact col-md-6 wow fadeIn delay-08s">
+                  <div class="col-md-10 col-md-offset-1">
+                    <div id="note"></div>
+                    <div id="sendmessage">Your message has been sent. Thank you!</div>
+                    <div id="errormessage"></div>
+                    <form action="" method="post" role="form" class="contactForm">
+                      <div class="form-group">
+                        <input type="text" name="name" class="form-control" id="name" placeholder="Your Name" data-rule="minlen:4" data-msg="Please enter at least 4 chars" />
+                        <div class="validation"></div>
+                      </div>
+                      <div class="form-group">
+                        <input type="email" class="form-control" name="email" id="email" placeholder="Your Email" data-rule="email" data-msg="Please enter a valid email" />
+                        <div class="validation"></div>
+                      </div>
+                      <div class="form-group">
+                        <input type="text" class="form-control" name="subject" id="subject" placeholder="Subject" data-rule="minlen:4" data-msg="Please enter at least 8 chars of subject" />
+                        <div class="validation"></div>
+                      </div>
+                      <div class="form-group">
+                        <textarea class="form-control" name="message" rows="5" data-rule="required" data-msg="Please write something for us" placeholder="Message"></textarea>
+                        <div class="validation"></div>
+                      </div>
+      
+                      <div class="text-center"><button type="submit" class="contact-submit">Send Message</button></div>
+                    </form>
+                  </div>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+        <footer class="footer">
+          <div class="container">
+            <div class="row bort">
+              </div>
+            </div>
+          </footer>
+
+
+
+
+
+
+
+
+
+
+
+          <div class="logo text-center" style="color:white">
+              <h2>Take a quick look at our current project<br> down below</h2>
+            </div>
+
+            <div class="devider"></div>
+
+
+
+
+
+
+
+
     
     <div class="TitleBar">
+
+      <img class="cgpslogo" src="{{ asset('/img/cgpsLogo.svg') }}" alt="">
+
       <div id="weather" style="width:20%; height:100%; "></div>
      
       
-        <div class="timer" id="timer"></div>
-        <button onclick="Pause()">Pause</button>
-        <button onclick="Turtle()">Turtle</button>
-        <button onclick="Cheeter()">Cheeter</button>
-
+      
         
     </div>
    
@@ -158,8 +315,18 @@
         <div id="Chart6" class="LineChart" >chart 6</div>
       </div>
     </div>
-    <div id="SolarStats" class="SolarStats" style="color: white;"></div>
-
+    
+    <div id="SolarStats" class="SolarStats">
+    
+        <div class="clock">
+            <div class="timer" id="timer"></div>
+            <img class="pause" src="{{ asset('/img/pause.svg') }}" alt=""onclick="Pause()">
+            <img class="play" src="{{ asset('/img/play.svg') }}" alt=""onclick="Turtle()">
+            <img class="speed" src="{{ asset('/img/speed.svg') }}" alt=""onclick="Cheeter()">
+          </div>
+    
+    </div>
+  </div>
 
 <div id="pricearray" style="background-color:red"></div>
 
@@ -173,5 +340,11 @@
   <script src="{{ asset('js/HighCharts.js') }}"></script>
   <script src="{{ asset('js/StatsViewer.js') }}"></script>
 <script src="{{ asset('js/MainPage.js') }}"></script>
-
+{{-- js entry Point --}}
+<script src="{{ asset('js/jquery.min.js') }}"></script>
+<script src="{{ asset('js/bootstrap.min.js') }}"></script>
+<script src="{{ asset('js/jquery.countdown.min.js') }}"></script>
+<script src="{{ asset('js/wow.js') }}"></script>
+<script src="{{ asset('js/custom.js') }}"></script>
+<script src="{{ asset('contactform/contactform.js') }}"></script>
 </html>
