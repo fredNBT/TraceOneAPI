96e589831c05e42cd569c9504b7b69b4f8106153
Lennart Beumer <len55@outlook.com>
2018-11-25 20:12:43 +0100

    circle radius change

--- public/js/createCircles.js
+++ public/js/createCircles.js
@@ -7,8 +7,13 @@ var circles = [];
 				id:         child.id,
 				value:			"",
 				text:       function(value){return value + '%';},
+<<<<<<< Updated upstream
 				radius:     25,
 				maxValue:	100,
+=======
+				radius:     30,
+				maxValue:		50,
+>>>>>>> Stashed changes
 				duration:   1000,
 				width:      6,
 				colors:     ['rgba(10, 84, 114, 0.75)','rgba(56, 175, 8, 0.5)']
